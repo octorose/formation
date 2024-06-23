f84accf7198e5dc7c07aceddf8e9773825990289
ocotorse <allouchhatim@gmail.com>
2024-06-23 19:46:45 +0100

    Tests of my urls

--- formationBackend/api/test.py
+++ formationBackend/api/test.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import Agent, Ligne
+from .models import Agent, Ligne, Personnel, Superviseur, Module, ResponsableEcoleFormation, Formateur
 
 class CustomTokenObtainPairViewTests(APITestCase):
 
@@ -108,6 +108,24 @@ class CreateSupervisorViewTests(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_supervisor_list(self):
+        print("Test: List supervisors.")
+        url = reverse('supervisor-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_supervisor_search(self):
+        print("Test: Search supervisors.")
+        url = reverse('supervisor-search')
+        response = self.client.get(url, {'query': 'supervisor'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_supervisor_delete(self):
+        print("Test: Delete supervisor.")
+        supervisor = Superviseur.objects.create(agent=self.agent, ligne=self.ligne)
+        url = reverse('delete_superviseur', kwargs={'pk': supervisor.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 class CreatePersonnelViewTests(APITestCase):
 
@@ -170,95 +188,71 @@ class CreatePersonnelViewTests(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-
-class CreateRHViewTests(APITestCase):
-
-    def setUp(self):
-        self.client = APIClient()
-        self.agent = Agent.objects.create_user(
-            username='admin',
-            email='admin@example.com',
-            role='Superviseur',
-            password='adminpassword',
-            prenom='Admin',
-            nom='User',
-            date_naissance='1980-01-01',
-            addresse='Admin Address',
-            cin='987654321',
-            numerotel='0987654321'
-        )
-        refresh = RefreshToken.for_user(self.agent)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
-
-    def test_create_rh(self):
-        print("Test: Create RH.")
-        url = reverse('create_rh')
-        data = {
-            'agent': {
-                'username': 'rhuser',
-                'email': 'rhuser@example.com',
-                'password': 'rhuserpassword',
-                'prenom': 'RH',
-                'nom': 'User',
-                'date_naissance': '1990-01-01',
-                'addresse': 'RH Address',
-                'cin': '123456789',
-                'numerotel': '1234567890'
-            },
-            'department': 'HR Department'
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_create_rh_without_token(self):
-        print("Test: Attempt to create RH without a token.")
-        url = reverse('create_rh')
-        data = {
-            'agent': {
-                'username': 'rhuser',
-                'email': 'rhuser@example.com',
-                'password': 'rhuserpassword',
-                'prenom': 'RH',
-                'nom': 'User',
-                'date_naissance': '1990-01-01',
-                'addresse': 'RH Address',
-                'cin': '123456789',
-                'numerotel': '1234567890'
-            },
-            'department': 'HR Department'
-        }
-        # Clearing credentials to simulate no token being provided
-        self.client.credentials()
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-
-class PersonnelCountByMonthAPIViewTests(APITestCase):
-
-    def setUp(self):
-        self.agent = Agent.objects.create_user(
-            username='admin',
-            email='admin@example.com',
-            role='Superviseur',
-            password='adminpassword',
-            prenom='Admin',
-            nom='User',
-            date_naissance='1980-01-01',
-            addresse='Admin Address',
-            cin='987654321',
-            numerotel='0987654321'
-        )
-        self.client = APIClient()
-        refresh = RefreshToken.for_user(self.agent)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
-
-    def test_get_personnel_count_by_month(self):
-        url = reverse('personnel-count-by-month')
+    def test_personnel_list(self):
+        print("Test: List personnel.")
+        url = reverse('personnel-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_personnel_sum_by_etat(self):
+        print("Test: Sum personnel by etat.")
+        url = reverse('personnel-sum-by-etat')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-if __name__ == '__main__':
-    import unittest
-    unittest.main()
+    def test_personnel_update(self):
+        print("Test: Update personnel.")
+        personnel = Personnel.objects.create(agent=self.agent, etat='Active')
+        url = reverse('update_personnel', kwargs={'pk': personnel.pk})
+        data = {
+            'agent': {
+                'username': 'updated_personnel',
+                'email': 'updated_personnel@example.com',
+                'password': 'updatedpassword',
+                'prenom': 'UpdatedPersonnel',
+                'nom': 'User',
+                'date_naissance': '1991-01-01',
+                'addresse': 'Updated Personnel Address',
+                'cin': '123456780',
+                'numerotel': '1234567891'
+            },
+            'etat': 'Inactive'
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class ModuleViewTests(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.agent = Agent.objects.create_user(
+            username='admin',
+            email='admin@example.com',
+            role='Superviseur',
+            password='adminpassword',
+            prenom='Admin',
+            nom='User',
+            date_naissance='1980-01-01',
+            addresse='Admin Address',
+            cin='987654321',
+            numerotel='0987654321'
+        )
+        refresh = RefreshToken.for_user(self.agent)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
+
+    def test_module_list(self):
+        print("Test: List modules.")
+        url = reverse('module-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_module_create(self):
+        print("Test: Create module.")
+        url = reverse('module-create')
+        data = {
+            'name': 'Test Module',
+            'description': 'Test Module Description'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
